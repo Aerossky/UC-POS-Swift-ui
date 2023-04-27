3fc6b81c13a2ea73f0074fba688d53c9234a3db1
Aerossky <riskyradeonrx560@gmail.com>
2023-04-27 19:05:37 +0700

    card for product done

--- UCWalkPOS/Components/ProductCard.swift
+++ UCWalkPOS/Components/ProductCard.swift
@@ -9,13 +9,53 @@ import SwiftUI
 
 struct ProductCard: View {
     
+    var product : Product
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .topTrailing) {
+            ZStack(alignment: .bottom){
+                Image(product.image)
+                    .resizable()
+                    .cornerRadius(20)
+                    .frame(width: 180)
+                    .scaledToFit()
+                
+                VStack(alignment: .leading){
+                    Text(product.name).bold()
+                    
+                    Text("RP. \(product.price)").font(.caption)
+                }
+                .padding()
+                .frame(width: 180, height: 60)
+                .background(.ultraThinMaterial)
+                .cornerRadius(15)
+                
+            }
+            .frame(width: 180, height: 250)
+        .shadow(radius: 3)
+            
+            Button{
+                
+            }label: {
+                Image(systemName: "plus")
+                    .padding(10)
+                    .foregroundColor(.white)
+                    .background(.black)
+                    .cornerRadius(50)
+                    .padding()
+                    .animation(.spring())
+                    .scaleEffect(1.1)
+                    .shadow(radius: 5)
+                
+                
+                   
+            }
+        }
     }
 }
 
 struct ProductCard_Previews: PreviewProvider {
     static var previews: some View {
-        ProductCard()
+        ProductCard(product: productList[0])
     }
 }
